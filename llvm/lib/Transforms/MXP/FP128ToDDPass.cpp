1aadc6532e73b112eb975f7f71f78eb9edd10402
Xing Su <xsu1989@foxmail.com>
2025-04-16 19:41:47 +0800

    update llvm/lib/Transforms/MXP/FP128ToDDPass.cpp

--- llvm/lib/Transforms/MXP/FP128ToDDPass.cpp
+++ llvm/lib/Transforms/MXP/FP128ToDDPass.cpp
@@ -43,7 +43,7 @@ inline void FP128ToDDPass::ChangeInstToCall(T *I,
 #endif
       auto *constant = dyn_cast<Constant>(arg);
       if (constant &&
-          arg->getType()->isFP128Ty()) { //如果函数参数中有fp128常量，先转成dd
+          arg->getType()->isPPC_FP128Ty()) { //如果函数参数中有fp128常量，先转成dd
         Function *FP128ToDD = M->getFunction("FP128ToDD");
         if (!FP128ToDD) {
           Type *fp128Type = Type::getFP128Ty(M->getContext());
@@ -140,8 +140,8 @@ bool FP128ToDDPass::runOnModule(Module &M) {
   for (auto &F : M) {
     for (auto &BB : F) {
       for (auto &I : BB) {
-        if (auto *SI = dyn_cast<StoreInst>(&I)) {
-          if (SI->getValueOperand()->getType()->isFP128Ty()) {
+        /*if (auto *SI = dyn_cast<StoreInst>(&I)) {
+          if (SI->getValueOperand()->getType()->isPPC_FP128Ty()) {
             // Get the fp128 constant
             auto *FP128Const = dyn_cast<ConstantFP>(SI->getValueOperand());
             if (!FP128Const)
@@ -173,12 +173,12 @@ bool FP128ToDDPass::runOnModule(Module &M) {
             //// Replace the fp128 constant with the double constant
             // SI->setOperand(0, DoubleConst);
           }
-        } else if (auto *BinOp = dyn_cast<BinaryOperator>(&I)) { // 处理计算指令
+        } else*/ if (auto *BinOp = dyn_cast<BinaryOperator>(&I)) { // 处理计算指令
           std::string funName = getBOName(BinOp);
-          if (BinOp->getType()->isFP128Ty() &&
+          if (BinOp->getType()->isPPC_FP128Ty() &&
               !funName.empty()) { //处理fp128的add,sub,mul,div
             for (unsigned i = 0; i < BinOp->getNumOperands(); ++i) {
-              if (BinOp->getOperand(i)->getType()->isFP128Ty()) {
+              if (BinOp->getOperand(i)->getType()->isPPC_FP128Ty()) {
                 funName += "2";
               } else if (BinOp->getOperand(i)->getType()->isDoubleTy()) {
                 funName += "1";
@@ -188,35 +188,35 @@ bool FP128ToDDPass::runOnModule(Module &M) {
           }
         } else if (I.getOpcode() == Instruction::FNeg) {
           Instruction *II = &I;
-          if (II->getType()->isFP128Ty()) {
-            CF1(II, "fp128_neg");
+          if (II->getType()->isPPC_FP128Ty()) {
+            CF1(II, "ppc_neg");
           }
         } else if (auto *FCmp = dyn_cast<FCmpInst>(&I)) { // 处理比较指令
-          if (FCmp->getOperand(0)->getType()->isFP128Ty()) {
+          if (FCmp->getOperand(0)->getType()->isPPC_FP128Ty()) {
             switch (FCmp->getPredicate()) {
             case FCmpInst::FCMP_OGT:
             case FCmpInst::FCMP_UGT:
-              CB2(FCmp, "fp128_gt");
+              CB2(FCmp, "ppc_gt");
               break;
             case FCmpInst::FCMP_OGE:
             case FCmpInst::FCMP_UGE:
-              CB2(FCmp, "fp128_ge");
+              CB2(FCmp, "ppc_ge");
               break;
             case FCmpInst::FCMP_OLT:
             case FCmpInst::FCMP_ULT:
-              CB2(FCmp, "fp128_lt");
+              CB2(FCmp, "ppc_lt");
               break;
             case FCmpInst::FCMP_OLE:
             case FCmpInst::FCMP_ULE:
-              CB2(FCmp, "fp128_le");
+              CB2(FCmp, "ppc_le");
               break;
             case FCmpInst::FCMP_OEQ:
             case FCmpInst::FCMP_UEQ:
-              CB2(FCmp, "fp128_eq");
+              CB2(FCmp, "ppc_eq");
               break;
             case FCmpInst::FCMP_ONE:
             case FCmpInst::FCMP_UNE:
-              CB2(FCmp, "fp128_ne");
+              CB2(FCmp, "ppc_ne");
               break;
             default:
               continue;
@@ -226,22 +226,22 @@ bool FP128ToDDPass::runOnModule(Module &M) {
           ////errs() << "Call I : " << *FCall << "\n";
           auto *Callee = FCall->getCalledFunction();
           if (Callee) {
-            if (Callee->getReturnType()->isFP128Ty()) {
+            if (Callee->getReturnType()->isPPC_FP128Ty()) {
               ////errs() << "FP128 Call : " << I << "\n";
-              if (Callee->getName() == "llvm.fmuladd.f128") {
+              if (Callee->getName() == "llvm.fmuladd.ppcf128") {
 #ifdef DEBUG
                 // errs() << "This call (" << I << ") need to deal." << "\n";
 #endif
-                std::string funName = "fp128_muladd";
+                std::string funName = "ppc_muladd";
                 for (unsigned i = 0; i < FCall->getNumOperands(); ++i) {
-                  if (FCall->getOperand(i)->getType()->isFP128Ty()) {
+                  if (FCall->getOperand(i)->getType()->isPPC_FP128Ty()) {
                     funName += "2";
                   } else if (FCall->getOperand(i)->getType()->isDoubleTy()) {
                     funName += "1";
                   }
                 }
                 CF3(FCall, funName);
-              } else if (Callee->getName() == "sinl") {
+              } /*else if (Callee->getName() == "sinl") {
                 CF1(FCall, "fp128_sin");
               } else if (Callee->getName() == "acosl") {
                 CF1(FCall, "fp128_acos");
@@ -249,7 +249,7 @@ bool FP128ToDDPass::runOnModule(Module &M) {
                 CF1(FCall, "fp128_sqrt");
               } else if (Callee->getName() == "expl") {
                 CF1(FCall, "fp128_exp");
-              }
+              }*/
             }
             if (Callee->getName() == "printf" &&
                 Callee->getFunctionType()->isVarArg()) {
@@ -267,7 +267,7 @@ bool FP128ToDDPass::runOnModule(Module &M) {
                 // errs() << "printf param : " << *pa << "\n";
 #endif
                 ////errs() << "This printf : " << *FCall << " need to deal.\n";
-                if (pa->getType()->isFP128Ty()) {
+                if (pa->getType()->isPPC_FP128Ty()) {
                   ////errs() << "fp128\n";
                   IRBuilder<> Builder(&I);
 #if 0
@@ -281,12 +281,12 @@ bool FP128ToDDPass::runOnModule(Module &M) {
 						auto *newPa = Builder.CreateCall(Fun, pa);
 						FCall->setArgOperand(i, newPa);
 #else
-                  Function *Fun = M.getFunction("fp128_printdd");
+                  Function *Fun = M.getFunction("ppc_printf");
                   if (!Fun) {
                     Fun = Function::Create(
                         FunctionType::get(Type::getInt32Ty(M.getContext()),
                                           {pa->getType()}, false),
-                        Function::ExternalLinkage, "fp128_printdd", &M);
+                        Function::ExternalLinkage, "ppc_printf", &M);
                   }
 
                   auto *newPa = Builder.CreateCall(Fun, pa);
@@ -307,7 +307,7 @@ bool FP128ToDDPass::runOnModule(Module &M) {
                   // }
                   // hasFP128Pa = true;
                   break;
-                } else if (pa->getType()->isDoubleTy()) {
+                } /*else if (pa->getType()->isDoubleTy()) {
                   ////errs() << "fp64\n";
                   // IRBuilder<> Builder(&I);
                   // Function *Fun = M.getFunction("fp128_printd");
@@ -347,7 +347,7 @@ bool FP128ToDDPass::runOnModule(Module &M) {
                   // erase.push_back(&I);
 
                   // break;
-                }
+                }*/
               }
               // if (hasFP128Pa) {
               //	//errs() << "This printf : " << *FCall << " need to
@@ -357,102 +357,102 @@ bool FP128ToDDPass::runOnModule(Module &M) {
             }
           }
         } else if (auto *CI = dyn_cast<CastInst>(&I)) {
-          // Type *SrcType = CI->getOperand(0)->getType();
-          // Type *DstType = CI->getType();
-          Type *SrcType = CI->getSrcTy();
-          Type *DstType = CI->getDestTy();
-#ifdef DEBUG
-          // errs() << "Cast Instruction : " << I << "\n";
-          // errs() << "Src Type : " << *SrcType << "\n";
-          // errs() << "Dst Type : " << *DstType << "\n";
-#endif
-          switch (CI->getOpcode()) {
-          case Instruction::FPTrunc:
-            if (SrcType->isFP128Ty() && DstType->isDoubleTy()) {
-              CT1(CI, "DDToDouble");
-            } else if (SrcType->isFP128Ty() && DstType->isFloatTy()) {
-              CT1(CI, "DDToFloat");
-            } /*else if (SrcType->isFloatTy() && DstType->isBFloatTy()) {
-                    CT1(CI, "FP32ToBF16");
-            } else if (SrcType->isDoubleTy() && DstType->isBFloatTy()) {
-                    CT1(CI, "FP64ToBF16");
-            }*/
-            else {
-              ////errs() << "Don't neet to change.\n";
-            }
-            break;
-
-          case Instruction::FPExt:
-            if (SrcType->isDoubleTy() && DstType->isFP128Ty()) {
-              CT1(CI, "DoubleToDD");
-            } else if (SrcType->isFloatTy() && DstType->isFP128Ty()) {
-              CT1(CI, "FloatToDD");
-            } /*else if (SrcType->isBFloatTy() && DstType->isFloatTy()) {
-                    CT1(CI, "BF16ToFP32");
-            } else if (SrcType->isBFloatTy() && DstType->isDoubleTy()) {
-                    CT1(CI, "BF16ToFP64");
-            } */
-            else {
-              ////errs() << "Don't neet to change.\n";
-            }
-            break;
-
-          case Instruction::FPToUI:
-            if (SrcType->isFP128Ty() && DstType->isIntegerTy()) {
-              if (auto *intTy = dyn_cast<IntegerType>(DstType)) {
-                unsigned bw = intTy->getIntegerBitWidth();
-                if (bw == 32) {
-                  CT1(CI, "DDToU32");
-                } else if (bw == 64) {
-                  CT1(CI, "DDToU64");
-                }
-              }
-            }
-            break;
-
-          case Instruction::FPToSI:
-            if (SrcType->isFP128Ty() && DstType->isIntegerTy()) {
-              if (auto *intTy = dyn_cast<IntegerType>(DstType)) {
-                unsigned bw = intTy->getIntegerBitWidth();
-                if (bw == 32) {
-                  CT1(CI, "DDToS32");
-                } else if (bw == 64) {
-                  CT1(CI, "DDToS64");
-                }
-              }
-            }
-            break;
-
-          case Instruction::UIToFP:
-            if (SrcType->isIntegerTy() && DstType->isFP128Ty()) {
-              if (auto *intTy = dyn_cast<IntegerType>(SrcType)) {
-                unsigned bw = intTy->getIntegerBitWidth();
-                if (bw == 32) {
-                  CT1(CI, "U32ToDD");
-                } else if (bw == 64) {
-                  CT1(CI, "U64ToDD");
-                }
-              }
-            }
-            break;
-
-          case Instruction::SIToFP:
-            if (SrcType->isIntegerTy() && DstType->isFP128Ty()) {
-              if (auto *intTy = dyn_cast<IntegerType>(SrcType)) {
-                unsigned bw = intTy->getIntegerBitWidth();
-                if (bw == 32) {
-                  CT1(CI, "S32ToDD");
-                } else if (bw == 64) {
-                  CT1(CI, "S64ToDD");
-                }
-              }
-            }
-            break;
-
-          default:
-            // BitCast Trunc ZExt SExt PtrToInt IntToPtr
-            break;
-          }
+//          // Type *SrcType = CI->getOperand(0)->getType();
+//          // Type *DstType = CI->getType();
+//          Type *SrcType = CI->getSrcTy();
+//          Type *DstType = CI->getDestTy();
+//#ifdef DEBUG
+//          // errs() << "Cast Instruction : " << I << "\n";
+//          // errs() << "Src Type : " << *SrcType << "\n";
+//          // errs() << "Dst Type : " << *DstType << "\n";
+//#endif
+//          switch (CI->getOpcode()) {
+//          case Instruction::FPTrunc:
+//            if (SrcType->isPPC_FP128Ty() && DstType->isDoubleTy()) {
+//              CT1(CI, "DDToDouble");
+//            } else if (SrcType->isPPC_FP128Ty() && DstType->isFloatTy()) {
+//              CT1(CI, "DDToFloat");
+//            } /*else if (SrcType->isFloatTy() && DstType->isBFloatTy()) {
+//                    CT1(CI, "FP32ToBF16");
+//            } else if (SrcType->isDoubleTy() && DstType->isBFloatTy()) {
+//                    CT1(CI, "FP64ToBF16");
+//            }*/
+//            else {
+//              ////errs() << "Don't neet to change.\n";
+//            }
+//            break;
+//
+//          case Instruction::FPExt:
+//            if (SrcType->isDoubleTy() && DstType->isPPC_FP128Ty()) {
+//              CT1(CI, "DoubleToDD");
+//            } else if (SrcType->isFloatTy() && DstType->isPPC_FP128Ty()) {
+//              CT1(CI, "FloatToDD");
+//            } /*else if (SrcType->isBFloatTy() && DstType->isFloatTy()) {
+//                    CT1(CI, "BF16ToFP32");
+//            } else if (SrcType->isBFloatTy() && DstType->isDoubleTy()) {
+//                    CT1(CI, "BF16ToFP64");
+//            } */
+//            else {
+//              ////errs() << "Don't neet to change.\n";
+//            }
+//            break;
+//
+//          case Instruction::FPToUI:
+//            if (SrcType->isPPC_FP128Ty() && DstType->isIntegerTy()) {
+//              if (auto *intTy = dyn_cast<IntegerType>(DstType)) {
+//                unsigned bw = intTy->getIntegerBitWidth();
+//                if (bw == 32) {
+//                  CT1(CI, "DDToU32");
+//                } else if (bw == 64) {
+//                  CT1(CI, "DDToU64");
+//                }
+//              }
+//            }
+//            break;
+//
+//          case Instruction::FPToSI:
+//            if (SrcType->isPPC_FP128Ty() && DstType->isIntegerTy()) {
+//              if (auto *intTy = dyn_cast<IntegerType>(DstType)) {
+//                unsigned bw = intTy->getIntegerBitWidth();
+//                if (bw == 32) {
+//                  CT1(CI, "DDToS32");
+//                } else if (bw == 64) {
+//                  CT1(CI, "DDToS64");
+//                }
+//              }
+//            }
+//            break;
+//
+//          case Instruction::UIToFP:
+//            if (SrcType->isIntegerTy() && DstType->isPPC_FP128Ty()) {
+//              if (auto *intTy = dyn_cast<IntegerType>(SrcType)) {
+//                unsigned bw = intTy->getIntegerBitWidth();
+//                if (bw == 32) {
+//                  CT1(CI, "U32ToDD");
+//                } else if (bw == 64) {
+//                  CT1(CI, "U64ToDD");
+//                }
+//              }
+//            }
+//            break;
+//
+//          case Instruction::SIToFP:
+//            if (SrcType->isIntegerTy() && DstType->isPPC_FP128Ty()) {
+//              if (auto *intTy = dyn_cast<IntegerType>(SrcType)) {
+//                unsigned bw = intTy->getIntegerBitWidth();
+//                if (bw == 32) {
+//                  CT1(CI, "S32ToDD");
+//                } else if (bw == 64) {
+//                  CT1(CI, "S64ToDD");
+//                }
+//              }
+//            }
+//            break;
+//
+//          default:
+//            // BitCast Trunc ZExt SExt PtrToInt IntToPtr
+//            break;
+//          }
         }
 #ifdef DEBUG
         // errs() << " BB : " << BB << "\n";
